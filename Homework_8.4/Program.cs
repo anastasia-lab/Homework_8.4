29fc3cccfce0c9b7bbf81049231fa879deb98391
anastasia-lab <anastasiya.shpilevaia@ictis.sfedu.ru>
2022-06-01 21:20:31 +0300

    Добавьте файлы проекта.

---
+++ Homework_8.4/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Homework_8._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument xDoc = new XDocument();
+            XElement people = new XElement("People");
+            XElement persons = new XElement("Person");
+
+            Console.Write("Введите ФИО человека: ");
+            string UserFIO = Console.ReadLine();
+            XAttribute xAttribute = new XAttribute("name", UserFIO);
+
+            XElement Adress = new XElement("Adress", "Адрес");
+
+            Console.Write("\nВведите улицу человека: ");
+            string UserStreet = Console.ReadLine();
+            XElement Street = new XElement("Street", UserStreet);
+
+            Console.Write("\nВведите номер дома человека: ");
+            int UserHouseNumber = int.Parse(Console.ReadLine());
+            XElement HouseNumber = new XElement("HouseNumber", UserHouseNumber);
+
+
+            Console.Write("\nВведите номер квартиры человека: ");
+            string UserFlatNumber = Console.ReadLine();
+            XElement FlatNumber = new XElement("FlatNumber", UserFlatNumber);
+
+            XElement Phones = new XElement("Phones", "Телефоны");
+
+            Console.Write("\nВведите номер мобильного телефона: ");
+            string UserMobilePhone = Console.ReadLine();
+            XElement MobilePhone = new XElement("MobilePhone", UserMobilePhone);
+
+            Console.Write("\nВведите номер домашнего телефон: ");
+            string UserFlatPhone = Console.ReadLine();
+            XElement FlatPhone = new XElement("FlatPhone", UserFlatPhone);
+
+            persons.Add(xAttribute);
+            persons.Add(Adress);
+            Adress.Add(Street);
+            Adress.Add(HouseNumber);
+            Adress.Add(FlatNumber);
+            persons.Add(Phones);
+            Phones.Add(MobilePhone);
+            Phones.Add(FlatPhone);
+
+            people.Add(persons);
+            xDoc.Add(people);
+            xDoc.Save("people.xml");
+
+            Console.WriteLine("Данные сохранены.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Homework_8.4/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Homework_8.4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Homework_8.4")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("9ab0c2da-099b-450f-b3fe-69b2a65dd226")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
